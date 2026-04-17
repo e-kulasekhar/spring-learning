1df9abbbf48ef4cec7c19ddd64759675306f5486
Kulasekhar <kulasekhar409@gmail.com>
2026-04-17 17:27:21 +0530

    Add Student class with setter injection

---
+++ 01-Setter-Injection/02-Setter-Object-Injection/Injection/Student.java
@@ -0,0 +1,32 @@
+package com.Pentagon;
+
+public class Student {
+
+    private Mobile m;
+    private int id;
+    private String name;
+
+    public Mobile getM() {
+        return m;
+    }
+
+    public void setM(Mobile m) {
+        this.m = m;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
